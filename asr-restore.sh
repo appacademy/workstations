652d9c5352d242e533d27a66f61dcceaf105f024
Jon Wolverton <wolverton.jr@gmail.com>
2016-01-03 14:59:16 -0800

    asr-restore: fix invisible terminal issue

--- asr-restore.sh
+++ asr-restore.sh
@@ -4,7 +4,7 @@ USAGE="\
 This script is designed to be invoked remotely.
 It opens the script in a terminal window so that
 the ASR can be monitored on that computer as it runs.
-It must be invoked as the root user."
+It must be invoked as the logged-in user."
 
 SCRIPT_DIR="$(cd "$(dirname $0)" && pwd)"
 TEMP_SCRIPT="$SCRIPT_DIR/.asr-restore-temp.sh"
@@ -12,7 +12,7 @@ PARTITION="/Volumes/AAStudent"
 SERVER_ADDR=$1
 
 if [[ -z "$1" ]]; then
-  echo 'You must provide the server IP address as an argument.' >&2 && exit 1
+ echo 'You must provide the server IP address as an argument.' >&2 && exit 1
 fi
 
 echo "
@@ -20,16 +20,16 @@ echo "
 #!/bin/bash
 cat $TEMP_SCRIPT
 
-asr restore --source asr://$SERVER_ADDR --target $PARTITION \
-            --erase --noprompt --noverify \
-  && bless -mount $PARTITION \
+sudo asr restore --source asr://$SERVER_ADDR --target $PARTITION \
+                 --erase --noprompt --noverify \
+  && sudo bless -mount $PARTITION \
   && echo 'Restore completed. Rebooting to restored partition.' \
-  && shutdown -r now
-
-rm '$TEMP_SCRIPT'
+  && sudo shutdown -r now
 ##### /script ######
 
 " > "$TEMP_SCRIPT"
 
 chmod u+x "$TEMP_SCRIPT"
 open -a Terminal "$TEMP_SCRIPT"
+sleep 3 # give time for the script to load before removing
+rm "$TEMP_SCRIPT"
