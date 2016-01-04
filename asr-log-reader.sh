f14ed419851142d7c51b709eefb6154354a74a35
Jon Wolverton <wolverton.jr@gmail.com>
2016-01-04 09:15:29 -0800

    change asr system to background ps w/ log

---
+++ asr-log-reader.sh
@@ -0,0 +1,5 @@
+echo 'This is the ASR log reader. Killing this program does not kill the ASR client.'
+echo ''
+
+cd "$(dirname $0)" # go to the script's dir
+tail -f .asr-restore.log
---
+++ asr-restore-remote.sh
@@ -0,0 +1,17 @@
+if [[ -z "$1" ]]; then
+ echo 'You must provide the server IP address as an argument.' >&2 && exit 1
+fi
+
+SERVER_ADDR=$1
+USAGE="\
+This script is designed to be invoked remotely.
+It opens the script in a terminal window so that
+the ASR can be monitored on that computer as it runs.
+It must be invoked as the root user."
+
+cd "$(dirname $0)" # go to the script's dir
+
+echo "--------------- $(date '+%D %T') ---------------" >> .asr-restore.log
+sudo -u jrw open -a Terminal asr-log-reader.sh
+
+./asr-restore.sh $SERVER_ADDR >> .asr-restore.log 2>&1
